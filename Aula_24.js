6605126847dceebf30d75950d20af3e131736a0c
DanielleMauro16 <eng.danielle.mauro16@gmail.com>
2022-10-24 19:23:28 -0300

    Fim da primeira parte da aula

--- Aula_24.js
+++ Aula_24.js
@@ -1,1 +1,50 @@
 //Aula 24-10-2022
+// Percorrer de Zero até o número de alunos
+
+//Se o número for par, escreva ‘par’ e o número correspondente
+
+//Se o número for ímpar, escreva ‘ímpar’ e o número correspondente  
+
+//Se o número for zero, escreva ‘zero’ e o número correspondente
+
+let numerodealunos = 10
+
+for (let contador = 0; contador <= numerodealunos; contador++) {
+
+    console.log (contador)
+}
+
+
+
+
+let numerodealunos = 10
+
+for (let contador = 0; contador <= numerodealunos; contador++) {
+
+    console.log (contador)
+
+    if (contador == 0 ){
+        console.log ("O número atual é zero")
+    }else if (contador % 2 ==0){
+            console.log ("O número é par")
+        }else{
+            console.log ("O número é ímpar")
+        }
+        }
+    
+        let numerodealunos = 10
+
+for (let contador = 0; contador <= numerodealunos; contador++) {
+
+    console.log (contador)
+
+    if (contador == 0 ){
+        console.log ("O número atual é zero")
+    }else if (contador % 2 ==0){
+            console.log ("O número " + contador + " é par")
+        }else{
+            console.log ("O número " + contador + " é ímpar")
+        }
+        }
+    
+
